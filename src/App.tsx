6d9f446cacf11c6a16e4b5a43ff001d8f79f6aa7
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-17 22:04:41 +0800

    Add size variant to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ function App() {
   return (
     <div className="container mx-auto px-4">
       <p className="text-slate-700">Primary button</p>
-      <Button intent={'primary'} onPress={handleClick}>
+      <Button intent="primary" size="small" onPress={handleClick}>
         Click
       </Button>
       <p className="text-slate-700">Secondary button</p>
-      <Button intent={'secondary'} onPress={handleClick}>
+      <Button intent="secondary" size="medium" onPress={handleClick}>
         Click
       </Button>
       <p className="text-slate-700">Destructive button</p>
-      <Button intent={'destructive'} onPress={handleClick}>
+      <Button intent="destructive" size="large" onPress={handleClick}>
         Click
       </Button>
     </div>
